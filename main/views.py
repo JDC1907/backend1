853aefda65d237d61a021c49c10831efa7252583
JDC1907 <jaaldiaz@espol.edu.ec>
2025-01-29 05:39:23 -0500

    cambios

---
+++ main/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+# Importe el decorador login_required
+from django.contrib.auth.decorators import login_required
+
+ # Importe el decorador login_required
+from django.contrib.auth.decorators import login_required, permission_required
+
+ # Importe requests y json
+import requests
+import json
+
+
+
+
+# Restricción de acceso con @login_required y permisos con @permission_required
+@login_required
+@permission_required('main.index_viewer', raise_exception=True)
+def index(request):
+     # Arme el endpoint del REST API
+     current_url = request.build_absolute_uri()
+     url = current_url + '/api/v1/landing'
+#
+     # Petición al REST API
+     response_http = requests.get(url)
+     response_dict = json.loads(response_http.content)
+
+     print("Endpoint ", url)
+     print("Response ", response_dict)
+
+     # Respuestas totales
+     total_responses = len(response_dict.keys())
+     # Valores de la respuesta
+     responses = response_dict.values()
+     # Objeto con los datos a renderizar
+     data = {
+         'title': 'Landing - Dashboard',
+         'total_responses': total_responses,
+         'responses': responses
+     }
+     #return HttpResponse("Hello, World!")
+     #return render(request, 'main/base.html')
+     # Renderización en la plantilla
+     return render(request, 'main/index.html', data)
